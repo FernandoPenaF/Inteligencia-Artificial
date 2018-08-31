543f139a065213b6000e363e5e820b00ceb43470
LUIS FERNANDO PEÑA FLORES <lpenaf@itam.mx>
2018-08-31 15:52:53 -0500

    Se estructuran las carpetas y se añaden archivos.
    Se añaden los archivos iniciales del proyecto1

--- Ejemplo11.pl
+++
@@ -1,5 +0,0 @@
-%consecutivos(i,i,i).
-
-consecutivos(A, B, [A,B|_]):-!.
-consecutivos(A,B,[_|R]):-
-    consecutivos(A,B,R).
--- Ejemplo6.pl
+++
@@ -1,9 +0,0 @@
-gusta(jorge, X):-
-    carne(X).
-gusta(beatriz,X):-
-    X==higado,!,fail.
-gusta(beatriz,X):-
-    carne(X).
-carne(pancita).
-carne(higado).
-carne(filete).
