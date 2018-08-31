f847f65a76d7c2cbe442d1d851f3cc38d8c18498
LUIS FERNANDO PEÑA FLORES <lpeaflo1@itam.mx>
2018-08-31 18:32:44 -0500

    Suma de listas implementada.

--- Miscelanea/Lista.pl
+++ Miscelanea/Lista.pl
@@ -13,6 +13,17 @@ count([_|Z], SUM):-
     count(Z, ACUM),
     SUM is ACUM + 1.
 
+cuantas_aparece(_, [], 0):-!.
+cuantas_aparece(DATA, [DATA|Z], SUM):-
+    cuantas_aparece(DATA, Z, ACUM),
+    SUM is ACUM + 1, !.
+cuantas_aparece(DATA, [_|Z], SUM):-
+    cuantas_aparece(DATA, Z, SUM).
+
+invierte_lista([],Z,Z).
+invierte_lista([X|Y],Z,ACUM):-
+    invierte_lista(Y,Z,[X|ACUM]).
+
 suma_lista([],0).
 suma_lista([X|Z], SUM):-
     suma_lista(Z, ACUM),
@@ -23,3 +34,9 @@ list_sum([Item], Item):-!.
 list_sum([Item1,Item2 | Tail], Total) :-
     SUM is Item1 + Item2,
     list_sum([SUM|Tail], Total).
+
+%Listas del mismo tamaño
+suma_listas([],[],[]).
+suma_listas([X|T],[Y|R],[SUM|L]):-
+    SUM is X + Y,
+    suma_listas(T,R,L).
