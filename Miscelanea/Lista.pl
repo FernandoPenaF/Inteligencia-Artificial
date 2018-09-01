89fd800153f6f754d570e88f7469ee222d59aab2
LUIS FERNANDO PEÑA FLORES <lpeaflo1@itam.mx>
2018-08-31 19:42:14 -0500

    Un intento de derivar

--- Miscelanea/Lista.pl
+++ Miscelanea/Lista.pl
@@ -3,6 +3,8 @@ pertenece_a(X, [_|Z]):-
     pertenece_a(X,Z).
 
 agrega_inicio(DATA,LIST,[DATA|LIST]).
+quita_primero([],[]).
+quita_primero([_|X],X).
 
 combina([],List, List):-!.
 combina([X|Lista1],Lista2,[X|Lista3]):-
@@ -35,8 +37,23 @@ list_sum([Item1,Item2 | Tail], Total) :-
     SUM is Item1 + Item2,
     list_sum([SUM|Tail], Total).
 
+
+
+
 %Listas del mismo tamaño
 suma_listas([],[],[]).
 suma_listas([X|T],[Y|R],[SUM|L]):-
     SUM is X + Y,
     suma_listas(T,R,L).
+
+resta_listas([],[],[]).
+resta_listas([X|T],[Y|R],[SUM|L]):-
+    SUM is X - Y,
+    resta_listas(T,R,L).
+
+deriva([],[],0).
+deriva([X|Z],L, SUM):-
+    deriva(Z,L,ACUM),
+    SUM is ACUM + 1,
+    COEFF is X*SUM,
+    write(COEFF),nl,write(SUM),nl.
