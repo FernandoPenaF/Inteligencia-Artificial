c243fa39dc1e69116ff2ec5d2f55748e931b4082
LUIS FERNANDO PEÑA FLORES <lpenaf@102Mac15.local>
2018-10-22 15:53:31 -0700

    Se añaden archivos que se borraron.
    ¿Quién sabe por qué?

---
+++ P2 - Metro/Algunas utilidades/Utils/src/utils/Utils.java
@@ -0,0 +1,100 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Utils {
+
+    /*
+    Se representan las conexiones como:
+        conexion(pino_suarez,merced, 0.79).%rosa
+        conexion(merced, candelaria,0.70).%rosa
+        conexion(candelaria, san_lazaro,0.59).%rosa
+    */
+    public static void readStations(String filename) throws FileNotFoundException, IOException{
+        FileReader fr = new FileReader(filename);
+        BufferedReader br = new BufferedReader(fr);
+        
+        ArrayList<ArrayList<String>> stations = new ArrayList<>();
+        HashSet <String> s = new HashSet<>();
+        String line, primera, segunda;
+        String[] ans;
+        int linea = -1;
+        
+        while ((line = br.readLine()) != null) {
+            if(line.startsWith("%")){
+                linea++;
+                stations.add(new ArrayList<>());
+                continue;
+            }
+            line = line.substring(9, line.length());
+            ans = line.split(",");
+            primera = ans[0].trim();
+            segunda = ans[1].trim();
+            
+            if(!s.contains(primera)){
+                s.add(primera);
+                stations.get(linea).add(primera);
+            }
+            if(!s.contains(segunda)){
+                s.add(segunda);
+                stations.get(linea).add(segunda);
+            }
+	}
+        
+        for (int i = 0; i < stations.size(); i++) {
+            ArrayList<String> ar = stations.get(i);
+            System.out.println("%Línea " + (i + 1));
+            for (int j = 0; j < ar.size(); j++) {
+                System.out.println("estacion(" + ar.get(j) + ", 1, 1).");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static ArrayList<Path> readUniqueRoutes(String filename) throws FileNotFoundException, IOException{
+        FileReader fr = new FileReader(filename);
+        BufferedReader br = new BufferedReader(fr);
+        
+        ArrayList<Path> routes = new ArrayList<>();
+        String line;
+        String[] ans;
+        Path p;
+        int i = 0;
+        
+        while ((line = br.readLine()) != null) {
+            i++;
+            ans = line.split(" ");
+            p = new Path(Double.valueOf(ans[1]),ans[0]);
+            if(!routes.contains(p))
+                routes.add(p);
+	}
+        System.out.println("Líneas leidas: " + i);
+        return routes;
+    }
+    
+    public static ArrayList<Path> sortList(ArrayList<Path> paths){
+        Collections.sort(paths, (Path p1, Path p2) -> {
+            return p1.compareTo(p2);
+        });
+        return paths;
+    }
+    
+    /**
+     * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+//        ArrayList<Path> s = Utils.readUniqueRoutes("file.txt");
+//        ArrayList<Path> sort = Utils.sortList(s);
+//        System.out.println("Caminos únicos: " + s.size());
+//        System.out.println("Camino más corto: " + sort.get(0).toString());
+//        System.out.println("Camino más largo: " + sort.get(sort.size() - 1).toString());
+//        Utils.readStations("conexiones.txt");
+    }   
+}
