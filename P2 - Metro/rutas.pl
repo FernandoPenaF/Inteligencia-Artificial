d4cc030733dc5fe533946567f59af011ace1bbac
LUIS FERNANDO PEÑA FLORES <lpeaflo1@itam.mx>
2018-10-25 17:32:10 -0500

    Se termina de comentar el programa

--- P2 - Metro/rutas.pl
+++ P2 - Metro/rutas.pl
@@ -2,6 +2,13 @@
 % Peña Flores, Luis Fernando     - 158488
 % Orduña Ferreira, Fabián        - 159001
 
+% Archivo de Prolog que implementa
+% la búsqueda A* para encontrar un
+% camino óptimo en la red del Sistema
+% de Transporte Colectivo Metro.
+
+% Es necesario tener cargado el archivo 'stcm'
+% que contiene la base de conocimiento del programa.
 :- ensure_loaded(stcm). %Carga la base de conocimiento
 
 % estaciones_conectadas(i,i,o).
@@ -76,14 +83,25 @@ agrega_rutas_a_cola_de_prioridad(Destino,Ruta,Cola,Resultado) :-
 % Dada una estación destino y una lista de estaciones
 % regresa una lista de las estaciones dadas ordenadas
 % según la función heuristica
-%
 % Se utiliza el algoritmo de Insertion Sort
+%
+% i: Estación destino
+% i: Lista de estaciones
+% o: Lista de estaciones ordenadas
 ordena(_,[],[]).
 ordena(Destino,[Cabeza|Cola],Resultado) :-
        ordena(Destino,Cola,ResP),
        agrega_orden(Destino,Cabeza,ResP,Resultado).
 
-%Métodos auxiliares para ordena(i,i,o).
+% ordena(i,i,i,o).
+% Dada una estación destino y una lista de estaciones
+% aplica el algoritmo Insertion Sort para ordenar las
+% estaciones.
+%
+% i: Estación destino
+% i: Lista de estaciones
+% i: Lista restante
+% o: Lista de estaciones ordenadas
 agrega_orden(_,Elem,[],[Elem]) :-!.
 agrega_orden(Destino,Elem,[X|Y],[Elem,X|Y]) :-
        heuristica(Destino,Elem,Res1),
@@ -92,34 +110,63 @@ agrega_orden(Destino,Elem,[X|Y],[Elem,X|Y]) :-
 agrega_orden(Destino,Elem,[X|Y],[X|Z]) :-
        agrega_orden(Destino,Elem,Y,Z),!.
 
-% este metodo calcula la heuristica de la situacion actual
-% i: nodo destino
-% i: lista del camino
-% o: valor calculado
+% heuristica(i,i,o).
+% Calcula la heuristica de la situacion actual
+% 
+% i: Estación destino
+% i: Lista de la ruta
+% o: Valor heurístico
 heuristica(Destino, [EstacionActual|CaminoRecorrido], ValorCalculado) :-
        longitud_ruta([EstacionActual|CaminoRecorrido],Res1),
        heuristica_geografica(EstacionActual,Destino,Res2),
        ValorCalculado is Res1 + Res2.
 
+% heuristica(i,i,o).
+% Calcula la heuristica de la situacion actual
+% Esta calculo determina que tan lejos se encuentra de
+% la estación destino.
+% 
+% i: Estación actual
+% i: Estación destino
+% o: Valor heurístico geográfico
 heuristica_geografica(EstacionOrigen,EstacionDestino,Resultado) :-
        estacion(EstacionOrigen,Coord1,Coord12),
        estacion(EstacionDestino,Coord2,Coord22),
        calcula_distancia(Coord1,Coord12,Coord2,Coord22,Resultado).
 
+% calcula_distancia(i,i,i,i,o).
+% Calcula la distancia entre dos estaciones
+% 
+% i: Latitud de estacion A
+% i: Longitud de estacion A
+% i: Latitud de estacion B
+% i: Longitud de estacion B
+% o: Distancia entre las estaciones
 calcula_distancia(Ll1,Ln1,Ll2,Ln2,Res) :-
        Res is sqrt( (Ll1-Ll2)**2 + (Ln1-Ln2)**2) * 110.57. %110.57,111.70
 
+% ruta_Aestrella(i,i,o).
+% Calcula una ruta entre dos estaciones dadas
+% Utiliza la búsqueda heurístico A*
+%
+% i: Estacion origen
+% i: Estacion destino
+% o: Ruta entre estación origen y estación destino
 ruta_Aestrella(Origen,Destino,Ruta) :-
        estacion(Origen,_,_),
        estacion(Destino,_,_),
        recorre_Aestrella(Destino,[Origen],[],Ruta).
 
-%Destino,Ruta actual, cola de prioridad,Resultado
+% recorre_Aestrella(i,i,i,o).
+% Dadas las estaciones adyacentes de una estación
+% se concatenan a la ruta actual y se agregan a la cola de
+% prioridad antes de seguir aplicando la búsqueda A*.
+%
+% i: Estacion destino
+% i: Ruta actual
+% i: Cola de prioridad
+% o: Ruta propuesta entre estación origen y estación destino
 recorre_Aestrella(Destino,[Destino|Resto],_,[Destino|Resto]) :-!.
-
-% aqui lo que tiene que hacer es llamar a todos los adyacentes,
-% concatenarlos a la ruta actual
-%agregarlos a la cola de prioridad y seguir recorriendo
 recorre_Aestrella(Destino,[CabezaRutaActual|ColaRutaActual],ColaDePrioridad,Resultado) :-
        %de los resultados de ruta actual con sus adyacentes       write("Bandera 1"),nl,
        estaciones_adyacentes(CabezaRutaActual,ResultadosAdyacentes),
@@ -132,7 +179,16 @@ recorre_Aestrella(Destino,[CabezaRutaActual|ColaRutaActual],ColaDePrioridad,Resu
        %write([CabezaColaActualizada|RestoColaActualizada]),nl,nl,
        recorre_Aestrella(Destino,CabezaColaActualizada,RestoColaActualizada,Resultado).
 
-%Destino,RutaActual,ListaAdyacentes,ColaDePrioridad,Res)
+% inserta_aux(i,i,i,i,o).
+% Método auxiliar que agrega las estaciones
+% adyacentes a la cola de prioridad en caso
+% de que aún no se encuentren en ella.
+%
+% i: Estacion destino
+% i: Ruta actual
+% i: Lista de estaciones adyacentes
+% i: Cola de prioridad
+% o: Cola de prioridad actualizada
 inserta_aux(_,_,[],ColaDePrioridadActualizada,ColaDePrioridadActualizada) :-!.
 inserta_aux(Destino,[CabezaRutaActual|RestoRutaActual],[CabezaListaAdyacentes],ColaPrioridad,Resultado):-
       (   \+member(CabezaListaAdyacentes,[CabezaRutaActual|RestoRutaActual]) ->
@@ -141,7 +197,6 @@ inserta_aux(Destino,[CabezaRutaActual|RestoRutaActual],[CabezaListaAdyacentes],C
       agrega_rutas_a_cola_de_prioridad(Destino,AInsertar,ColaPrioridad,ResP),
           inserta_aux(_,_,[],ResP,Resultado)
       ;inserta_aux(_,_,[],ColaPrioridad,Resultado)),!.
-
 inserta_aux(Destino,RutaActual,[CabezaListaAdyacentes|RestoListaAdyacentes],ColaPrioridad,Resultado) :-
        %nl,nl,nl,write(ColaPrioridad),nl,nl,nl,
        %nl,nl,write("Inserta a adyacentes"),
@@ -160,6 +215,11 @@ inserta_aux(Destino,RutaActual,[CabezaListaAdyacentes|RestoListaAdyacentes],Cola
         inserta_aux(Destino,RutaActual,RestoListaAdyacentes,ColaPrioridad,Resultado)
        ).
 
+% imprime(i).
+% imprime línea a línea el contenido
+% de una lista.
+%
+% i: Lista
 imprime([]) :-!.
 imprime([Ultimo]) :-
        write(Ultimo).
@@ -168,6 +228,15 @@ imprime([Cabeza|Resto]) :-
        nl,
        imprime(Resto).
 
+% calcula_ruta_optima(i,i,o).
+% Calcula una ruta entre dos estaciones dadas.
+% Además imprime la ruta, su longitud y su
+% número de estaciones.
+% Utiliza la búsqueda heurística A*
+%
+% i: Estacion origen
+% i: Estacion destino
+% o: Ruta entre estación origen y estación destino
 calcula_ruta_optima(Origen,Destino,RutaDesdeInicio) :-
        ruta_Aestrella(Origen,Destino,RutaI),!,
        reverse(RutaI,RutaDesdeInicio),
