f44363e453ab3fe03148fc2c58298e6fb1d50c31
LUIS FERNANDO PEÑA FLORES <lpeaflo1@itam.mx>
2018-11-22 19:20:43 -0600

    Se añaden archivos iniciales para proyecto 3

---
+++ P3 - Mini Router/miscelanea.pl
@@ -0,0 +1,119 @@
+% Malvaes Díaz, Stephanie Lizeth - 135515
+% Peña Flores, Luis Fernando     - 158488
+% Orduña Ferreira, Fabián        - 159001
+
+% IMPORTANTE: El working_directory de Prolog debe estar en
+% la carpeta de este archivo para no tener problemas.
+% En caso contrario, el archivo proyecto.pl contiene todo junto
+% para su correcta ejecuci�n.
+
+% Archivo de SWI Prolog que implementa
+% métodos interesantes para aplicar
+% a la red del Sistema Transporte
+% Colectivo Metro.
+
+% Es necesario tener cargado el archivo 'stcm.pl'
+% que contiene la base de conocimiento del programa.
+% Así como métodos auxilares del archivo 'rutas.pl'
+:- ensure_loaded(stcm). %Carga la base de conocimiento
+:- ensure_loaded(rutas). %Carga métodos auxilares principales
+
+% ruta(i,i,o,o).
+% Encuentra una ruta y la longitud
+% de dicho ruta entre dos estaciones
+% dadas, A y B.
+%
+% i: Estación A
+% i: Estación B
+% o: Ruta de A a B
+% o: Longitud de la ruta de A a B
+ruta(A,B,Path,Len) :-
+       recorre(A,B,[A],Q),
+       reverse(Q,Path),
+       longitud_ruta(Path,Len).
+
+% recorre(i,i,i,o).
+% Aplica búsqueda en profundidad para
+% encontrar un camino de A a B.
+%
+% i: Estación A
+% i: Estación B
+% i: Lista de estaciones visitadas
+% o: Ruta de A a B
+recorre(A,B,P,[B|P]) :-
+       estaciones_conectadas(A,B,_).
+recorre(A,B,Visited,Path) :-
+       estaciones_conectadas(A,C,_),
+       C \== B,
+       \+member(C,Visited),
+       recorre(C,B,[C|Visited],Path).
+
+% rutas(i,i).
+% Imprime todas las rutas entre A y B.
+%
+% i: Estación A
+% i: Estación B
+rutas(A, B):-
+	ruta(A, B, X, _),
+	write(X),
+	nl,
+	fail.
+
+% escribe_rutas(i,i).
+% Escribe en un archivo todas
+% las rutas entre A y B.
+%
+% i: Estación A
+% i: Estación B
+escribe_rutas(A, B) :-
+    open('file.txt', write, Stream),
+    ( ruta(A, B, X, L), write(Stream, X), write(Stream," "),
+	write(Stream, L), nl(Stream), fail; true ),
+    close(Stream).
+
+% estacion_a_menor_distancia(i,o,o).
+% El siguiente predicado regresa la estación adyacente, según la lista
+% de estaciones adyacentes que recibe,que está a la menor distancia de
+% la estacion dada.
+%
+% i: Estacion de la que se quiere obtener la estación a menor distancia
+% o: Nombre de la estacion adyacente a distancia menor
+% o: Distancia a dicha estacion adyacente
+estacion_a_menor_distancia(EstacionInicio,EstacionResultado,Distancia):-
+       estaciones_adyacentes(EstacionInicio, ListaAdyacentes),
+       estacion_menor(EstacionInicio,ListaAdyacentes,EstacionResultado,Distancia).
+
+% estacion_menor(i,i,o,o).
+% Dada una lista de estaciones, regresa la que tenga una
+% distancia menor y dicha distancia.
+%
+% i: Estacion actual
+% i: Lista de estaciones adyacentes
+% o: Nombre de la estacion adyacente a distancia menor
+% o: Distancia a dicha estacion adyacente
+estacion_menor(Inicio,[],Inicio,0):-!.
+estacion_menor(Inicio,[A],A, Dist):-
+       estaciones_conectadas(Inicio,A,Dist),!.
+estacion_menor(Inicio,[Primera|Cola],Estacion,Res):-
+       estacion_menor_aux(Inicio,Primera,Cola,Estacion,Res),!.
+
+% estacion_menor(i,i,i,o,o).
+% Método auxiliar que compara entre estaciones
+% para encontrar la de menor longitud.
+%
+% i: Estacion actual
+% i: Estacion actual con menor distancia
+% i: Lista de estaciones adyacentes
+% o: Nombre de la estacion adyacente a distancia menor
+% o: Distancia a dicha estacion adyacente
+estacion_menor_aux(EstacionInicial,EstacionMenorActual,[],EstacionMenorActual,Distancia):-
+          estaciones_conectadas(EstacionInicial,EstacionMenorActual,Distancia),!.
+estacion_menor_aux(EstacionInicial,EstacionMenorActual,[EstacionAPrueba|Resto],Estacion,Res):-
+      estaciones_conectadas(EstacionInicial,EstacionMenorActual,J),
+      DistanciaActual is J,
+      estaciones_conectadas(EstacionInicial, EstacionAPrueba,K),
+      DistanciaAComparar is K,
+      (   DistanciaActual > DistanciaAComparar ->
+      estacion_menor_aux(EstacionInicial,EstacionAPrueba,Resto,Estacion,Res);(
+      estacion_menor_aux(EstacionInicial,EstacionMenorActual,Resto,Estacion,Res)
+                              )).
---
+++ P3 - Mini Router/stcm.pl
@@ -0,0 +1,416 @@
+% Malvaes Díaz, Stephanie Lizeth - 135515
+% Peña Flores, Luis Fernando     - 158488
+% Orduña Ferreira, Fabián        - 159001
+
+% IMPORTANTE: El working_directory de Prolog debe estar en
+% la carpeta de este archivo para no tener problemas.
+% En caso contrario, el archivo proyecto.pl contiene todo junto
+% para su correcta ejecución.
+
+% Base de conocimiento del Sistema de Transporte Colectivo Metro
+% Contiene sus estaciones y sus conexiones
+
+% Las estaciones son representadas como:
+%	estacion(nombre,latitud,longitud).
+% Las conexiones son representadas como:
+%	conexion(estacionA,estacionB,distancia).
+
+%Línea 1
+estacion(observatorio,19.39833333,-99.20027778).
+estacion(tacubaya,19.40333333,-99.18722222).
+estacion(juanacatlan,19.41277778,-99.18222222).
+estacion(chapultepec,19.42083333,-99.17638889).
+estacion(sevilla,19.42194444,-99.17055556).
+estacion(insurgentes,19.42333333,-99.16305556).
+estacion(cuauhtemoc,19.42583333,-99.15472222).
+estacion(balderas,19.4275,-99.14916667).
+estacion(salto_del_agua,19.42694444,-99.14222222).
+estacion(isabel_la_catolica,19.42666667,-99.13777778).
+estacion(pino_suarez,19.42527778,-99.13305556).
+estacion(merced,19.42555556,-99.12472222).
+estacion(candelaria,19.42888889,-99.11944444).
+estacion(san_lazaro,19.43027778,-99.11472222).
+estacion(moctezuma,19.42722222,-99.11027778).
+estacion(balbuena,19.42333333,-99.10222222).
+estacion(boulevard_puerto_aereo,19.41972222,-99.09583333).
+estacion(gomez_farias,19.41638889,-99.09027778).
+estacion(zaragoza,19.41222222,-99.0825).
+estacion(pantitlan,19.41527778,-99.07222222).
+
+%Línea 2
+estacion(cuatro_caminos,19.45972222,-99.21583333).
+estacion(panteones,19.45888889,-99.20305556).
+estacion(tacuba,19.45944444,-99.18916667).
+estacion(cuitlahuac,19.4575,-99.18194444).
+estacion(popotla,19.45222222,-99.17472222).
+estacion(colegio_militar,19.44944444,-99.17222222).
+estacion(normal,19.44444444,-99.1675).
+estacion(san_cosme,19.44166667,-99.16111111).
+estacion(revolucion,19.43944444,-99.15555556).
+estacion(hidalgo,19.43722222,-99.14722222).
+estacion(bellas_artes,19.43611111,-99.14194444).
+estacion(allende,19.43555556,-99.1375).
+estacion(zocalo,19.43333333,-99.13277778).
+estacion(san_antonio_abad,19.41861111,-99.13416667).
+estacion(chabacano,19.40833333,-99.13583333).
+estacion(viaducto,19.40083333,-99.13694444).
+estacion(xola,19.39527778,-99.13777778).
+estacion(villa_de_cortes,19.3875,-99.13888889).
+estacion(nativitas,19.37944444,-99.14027778).
+estacion(portales,19.36972222,-99.14166667).
+estacion(ermita,19.36194444,-99.14305556).
+estacion(general_anaya,19.35333333,-99.145).
+estacion(tasqueña,19.34416667,-99.14277778).
+
+%Línea 3
+estacion(indios_verdes,19.49527778,-99.11944444).
+estacion(deportivo_18_de_marzo,19.48361111,-99.12666667).
+estacion(potrero,19.47694444,-99.13222222).
+estacion(la_raza,19.47027778,-99.13694444).
+estacion(tlatelolco,19.455,-99.14277778).
+estacion(guerrero,19.44527778,-99.14527778).
+estacion(juarez,19.43305556,-99.14777778).
+estacion(niños_heroes,19.41944444,-99.15055556).
+estacion(hospital_general,19.41361111,-99.15388889).
+estacion(centro_medico,19.40666667,-99.15583333).
+estacion(etiopia,19.39555556,-99.15638889).
+estacion(eugenia,19.38555556,-99.1575).
+estacion(division_del_norte,19.38,-99.15888889).
+estacion(zapata,19.37083333,-99.165).
+estacion(coyoacan,19.36138889,-99.17083333).
+estacion(viveros,19.35361111,-99.17611111).
+estacion(miguel_angel_de_quevedo,19.34638889,-99.18111111).
+estacion(copilco,19.33583333,-99.17666667).
+estacion(universidad,19.32444444,-99.17388889).
+
+%Línea 4
+estacion(martin_carrera,19.485,-99.10444444).
+estacion(talisman,19.47416667,-99.10805556).
+estacion(bondojito,19.46472222,-99.11194444).
+estacion(consulado,19.45777778,-99.11388889).
+estacion(canal_del_norte,19.44916667,-99.11638889).
+estacion(morelos,19.44916667,-99.11638889).
+estacion(fray_servando,19.42166667,-99.12055556).
+estacion(jamaica,19.40888889,-99.12222222).
+estacion(santa_anita,19.40277778,-99.12166667).
+
+%Línea 5
+estacion(politecnico,19.50083333,-99.14916667).
+estacion(instituto_del_petroleo,19.48944444,-99.14527778).
+estacion(autobuses_del_norte,19.47888889,-99.14055556).
+estacion(misterios,19.46333333,-99.13083333).
+estacion(valle_gomez,19.45861111,-99.11916667).
+estacion(eduardo_molina,19.45138889,-99.10555556).
+estacion(aragon,19.45111111,-99.09583333).
+estacion(oceania,19.44555556,-99.08722222).
+estacion(terminal_aerea,19.43361111,-99.08777778).
+estacion(hangares,19.42416667,-99.0875).
+
+%Línea 6
+estacion(el_rosario,19.50444444,-99.2).
+estacion(tezozomoc,19.495,-99.19638889).
+estacion(azcapotzalco,19.49111111,-99.18638889).
+estacion(ferreria,19.49083333,-99.17388889).
+estacion(norte_45,19.48861111,-99.16277778).
+estacion(vallejo,19.49055556,-99.15583333).
+estacion(lindavista,19.48777778,-99.13472222).
+estacion(la_villa_basilica,19.48138889,-99.11805556).
+
+%Línea 7
+estacion(aquiles_serdan,19.49055556,-99.195).
+estacion(camarones,19.47916667,-99.19).
+estacion(refineria,19.47,-99.19055556).
+estacion(san_joaquin,19.44583333,-99.19194444).
+estacion(polanco,19.43361111,-99.19111111).
+estacion(auditorio,19.42555556,-99.19194444).
+estacion(constituyentes,19.41194444,-99.19138889).
+estacion(san_pedro_de_los_pinos,19.39138889,-99.18611111).
+estacion(san_antonio,19.38472222,-99.18638889).
+estacion(mixcoac,19.37583333,-99.1875).
+estacion(barranca_del_muerto,19.36055556,-99.19027778).
+
+%Línea 8
+estacion(garibaldi_lagunilla,19.44444444,-99.13972222).
+estacion(san_juan_de_letran,19.43138889,-99.14166667).
+estacion(doctores,19.42166667,-99.14333333).
+estacion(obrera,19.41361111,-99.14416667).
+estacion(la_viga,19.40638889,-99.12611111).
+estacion(coyuya,19.39861111,-99.11361111).
+estacion(iztacalco,19.38861111,-99.11222222).
+estacion(apatlaco,19.37916667,-99.10972222).
+estacion(aculco,19.37416667,-99.10805556).
+estacion(escuadron_201,19.365,-99.10944444).
+estacion(atlalilco,19.35611111,-99.10138889).
+estacion(iztapalapa,19.35777778,-99.09333333).
+estacion(cerro_de_la_estrella,19.35611111,-99.08555556).
+estacion(uamI,19.35111111,-99.07472222).
+estacion(constitucion_de_1917,19.34583333,-99.06388889).
+
+%Línea 9
+estacion(puebla,19.40722222,-99.0825).
+estacion(ciudad_deportiva,19.40833333,-99.09111111).
+estacion(velodromo,19.40861111,-99.10305556).
+estacion(mixiuhca,19.40861111,-99.11277778).
+estacion(lazaro_cardenas,19.40694444,-99.145).
+estacion(chilpancingo,19.40583333,-99.16861111).
+estacion(patriotismo,19.40611111,-99.17888889).
+
+%Línea A
+estacion(agricola_oriental,19.40472222,-99.07).
+estacion(canal_de_san_juan,19.39861111,-99.05944444).
+estacion(tepalcates,19.39138889,-99.04638889).
+estacion(guelatao,19.38527778,-99.03583333).
+estacion(peñon_viejo,19.37333333,-99.01722222).
+estacion(acatitla,19.36472222,-99.00583333).
+estacion(santa_marta,19.36,-98.995).
+estacion(los_reyes,19.35888889,-98.97694444).
+estacion(la_paz,19.35055556,-98.96083333).
+
+%Línea B
+estacion(buenavista,19.44666667,-99.15333333).
+estacion(lagunilla,19.44333333,-99.13138889).
+estacion(tepito,19.4425,-99.12333333).
+estacion(flores_magon,19.43666667,-99.10361111).
+estacion(romero_rubio,19.44083333,-99.09444444).
+estacion(deportivo_oceania,19.45111111,-99.07916667).
+estacion(bosque_de_aragon,19.45805556,-99.06916667).
+estacion(villa_de_aragon,19.46166667,-99.06166667).
+estacion(nezahualcoyotl,19.47305556,-99.05444444).
+estacion(impulsora,19.48583333,-99.04888889).
+estacion(rio_de_los_remedios,19.49083333,-99.04666667).
+estacion(muzquiz,19.50166667,-99.04194444).
+estacion(ecatepec,19.51527778,-99.03611111).
+estacion(olimpica,19.52138889,-99.03333333).
+estacion(plaza_aragon,19.52833333,-99.03).
+estacion(ciudad_azteca,19.53472222,-99.0275).
+
+%Línea 12
+estacion(insurgentes_sur,19.37361111,-99.17888889).
+estacion(hospital_20_de_noviembre,19.37194444,-99.17111111).
+estacion(parque_de_los_venados,19.37083333,-99.15861111).
+estacion(eje_central,19.36138889,-99.15138889).
+estacion(mexicaltzingo,19.35777778,-99.12166667).
+estacion(culhuacan,19.33694444,-99.10888889).
+estacion(san_andres_tomatlan,19.32833333,-99.105).
+estacion(lomas_estrella,19.32222222,-99.09611111).
+estacion(calle_11,19.32055556,-99.08583333).
+estacion(periferico_oriente,19.31777778,-99.07472222).
+estacion(tezonco,19.30611111,-99.065).
+estacion(olivos,19.30416667,-99.05972222).
+estacion(nopalera,19.3,-99.04666667).
+estacion(zapotitlan,19.29666667,-99.03444444).
+estacion(tlaltenco,19.29416667,-99.02388889).
+estacion(tlahuac,19.28638889,-99.01416667).
+
+%Línea 1
+conexion(observatorio, tacubaya,1.46).%rosa
+conexion(tacubaya, juanacatlan,1.4).%rosa
+conexion(juanacatlan,chapultepec,1.16).%rosa
+conexion(chapultepec, sevilla,0.68).%rosa
+conexion(sevilla, insurgentes,0.82).%rosa
+conexion(insurgentes, cuauhtemoc, 0.95).%rosa
+conexion(cuauhtemoc, balderas,0.62).%rosa
+conexion(balderas, salto_del_agua,0.64).%rosa
+conexion(salto_del_agua,isabel_la_catolica,0.62).%rosa
+conexion(isabel_la_catolica, pino_suarez,0.57).%rosa
+conexion(pino_suarez,merced, 0.79).%rosa
+conexion(merced, candelaria,0.70).%rosa
+conexion(candelaria, san_lazaro,0.59).%rosa
+conexion(san_lazaro, moctezuma, 0.82).%rosa
+conexion(moctezuma, balbuena, 0.94).%rosa
+conexion(balbuena,boulevard_puerto_aereo,0.86).%rosa
+conexion(boulevard_puerto_aereo,gomez_farias,0.76).%rosa
+conexion(gomez_farias, zaragoza, 1.06).%rosa
+conexion(zaragoza, pantitlan,1.32).%rosa
+
+%Línea 2
+conexion(cuatro_caminos, panteones, 1.74). %azul
+conexion(panteones, tacuba, 1.64). %azul
+conexion(tacuba, cuitlahuac, 0.84). %azul
+conexion(cuitlahuac, popotla, 0.86). %azul
+conexion(popotla, colegio_militar, 0.68). %azul
+conexion(colegio_militar, normal, 0.69). %azul
+conexion(normal, san_cosme, 0.75). %azul
+conexion(san_cosme, revolucion, 0.86). %azul
+conexion(revolucion, hidalgo, 0.75). %azul
+conexion(hidalgo, bellas_artes, 0.63). %azul
+conexion(bellas_artes, allende, 0.49). %azul
+conexion(allende, zocalo, 0.72). %azul
+conexion(zocalo, pino_suarez, 0.8). %azul
+conexion(pino_suarez, san_antonio_abad, 1.1). %azul
+conexion(san_antonio_abad, chabacano, 0.79). %azul
+conexion(chabacano, viaducto, 0.91). %azul
+conexion(viaducto, xola, 0.65). %azul
+conexion(xola, villa_de_cortes, 0.85). %azul
+conexion(villa_de_cortes, nativitas, 0.91). %azul
+conexion(nativitas, portales, 1.08). %azul
+conexion(portales, ermita, 0.89). %azul
+conexion(ermita, general_anaya, 0.99). %azul
+conexion(general_anaya, tasqueña, 1.5). %azul
+
+% Línea 3
+% En Etiopia - Plaza de la Transparencia se consideró
+% solamente como Etiopia
+% Analogamente, Viveros - Derechos humanos se consideró
+% solamente como Viveros
+conexion(indios_verdes, deportivo_18_de_marzo, 1.42). %verde
+conexion(deportivo_18_de_marzo, potrero, 1.08). %verde
+conexion(potrero, la_raza, 1.11). %verde
+conexion(la_raza, tlatelolco, 1.6). %verde
+conexion(tlatelolco, guerrero, 1.18). %verde
+conexion(guerrero, hidalgo, 0.91). %verde
+conexion(hidalgo, juarez, 0.42). %verde
+conexion(juarez, balderas, 0.75). %verde
+conexion(balderas, niños_heroes, 0.86). %verde
+conexion(niños_heroes, hospital_general, 0.71). %verde
+conexion(hospital_general, centro_medico, 0.72). %verde
+conexion(centro_medico, etiopia, 1.3). %verde
+conexion(etiopia, eugenia, 1.1). %verde
+conexion(eugenia, division_del_norte, 0.87). %verde
+conexion(division_del_norte, zapata, 1.05). %verde
+conexion(zapata, coyoacan, 1.22). %verde
+conexion(coyoacan, viveros, 0.98). %verde
+conexion(viveros, miguel_angel_de_quevedo, 1.02). %verde
+conexion(miguel_angel_de_quevedo, copilco, 1.25). %verde
+conexion(copilco, universidad, 1.42). %verde
+
+%Línea 4
+conexion(martin_carrera, talisman, 1.3). %verde claro
+conexion(talisman, bondojito, 1.2). %verde claro
+conexion(bondojito, consulado, 0.94). %verde claro
+conexion(consulado, canal_del_norte, 0.99). %verde claro
+conexion(canal_del_norte, morelos, 1.07). %verde claro
+conexion(morelos, candelaria,1.25). %verde claro
+conexion(candelaria, fray_servando, 0.93). %verde claro
+conexion(fray_servando, jamaica, 1.33). %verde claro
+conexion(jamaica, santa_anita, 0.83). %verde claro
+
+%Línea 5
+conexion(politecnico, instituto_del_petroleo,1.32).%amarilla
+conexion(instituto_del_petroleo, autobuses_del_norte,1.35).%amarilla
+conexion(autobuses_del_norte, la_raza,1.17).%amarilla
+conexion(la_raza,misterios,0.95).%amarilla
+conexion(misterios, valle_gomez,1.28).%amarilla
+conexion(valle_gomez, consulado,0.84).%amarilla
+conexion(consulado, eduardo_molina,1.05).%amarilla
+conexion(eduardo_molina, aragon,1.09).%amarilla
+conexion(aragon, oceania, 1.21).%amarilla
+conexion(oceania, terminal_aerea,1.42).%amarilla
+conexion(terminal_aerea, hangares, 1.18).%amarilla
+conexion(hangares, pantitlan,1.69).%amarilla
+
+%Línea 6
+% Se toma solo como ferreria la estacion ferreria / arena ciudad de
+% mexico
+conexion(el_rosario, tezozomoc, 1.16). %rojo
+conexion(tezozomoc, azcapotzalco, 1.13).%rojo
+conexion(azcapotzalco, ferreria, 1.39).%rojo
+conexion(ferreria, norte_45, 1.33).%rojo
+conexion(norte_45, vallejo, 0.84).%rojo
+conexion(vallejo,instituto_del_petroleo,0.88).%rojo
+conexion(instituto_del_petroleo, lindavista, 1.35).%rojo
+conexion(lindavista, deportivo_18_de_marzo, 1.05).%rojo
+conexion(deportivo_18_de_marzo, la_villa_basilica, 0.85).%rojo
+conexion(la_villa_basilica, martin_carrera, 1.29).%rojo
+
+%Línea 7
+conexion(el_rosario, aquiles_serdan, 1.82).%naranja
+conexion(aquiles_serdan, camarones, 1.24).%naranja
+conexion(camarones, refineria, 1.11).%naranja
+conexion(refineria, tacuba, 1.2).%naranja
+conexion(tacuba, san_joaquin,1.54).%naranja
+conexion(san_joaquin, polanco,1.35).%naranja
+conexion(polanco, auditorio,0.9).%naranja
+conexion(auditorio, constituyentes,1.56).%naranja
+conexion(constituyentes, tacubaya, 1.13).%naranja
+conexion(tacubaya, san_pedro_de_los_pinos,1.27).%naranja
+conexion(san_pedro_de_los_pinos, san_antonio,0.71).%naranja
+conexion(san_antonio, mixcoac,0.99).%naranja
+conexion(mixcoac, barranca_del_muerto,1.69).%naranja
+
+%Línea 8
+conexion(garibaldi_lagunilla, bellas_artes, 0.80). %verde oscuro
+conexion(bellas_artes, san_juan_de_letran, 0.65). %verde oscuro
+conexion(san_juan_de_letran, salto_del_agua, 0.46). %verde oscuro
+conexion(salto_del_agua, doctores, 0.77). %verde oscuro
+conexion(doctores, obrera, 0.85). %verde oscuro
+conexion(obrera, chabacano, 0.99). %verde oscuro
+conexion(chabacano, la_viga, 1.02). %verde oscuro
+conexion(la_viga, santa_anita, 0.66). %verde oscuro
+conexion(santa_anita, coyuya, 1.22). %verde oscuro
+conexion(coyuya, iztacalco, 1.04). %verde oscuro
+conexion(iztacalco, apatlaco, 1.09). %verde oscuro
+conexion(apatlaco, aculco, 0.69). %verde oscuro
+conexion(aculco, escuadron_201, 1.04). %verde oscuro
+conexion(escuadron_201, atlalilco, 1.35). %verde oscuro
+conexion(atlalilco, iztapalapa, 0.86). %verde oscuro
+conexion(iztapalapa, cerro_de_la_estrella, 1.91). %verde oscuro
+conexion(cerro_de_la_estrella, uamI, 1.25). %verde oscuro
+conexion(uamI, constitucion_de_1917, 1.27). %verde oscuro
+
+%Línea 9
+conexion(pantitlan, puebla,1.55).%cafe
+conexion(puebla, ciudad_deportiva, 0.93).%cafe
+conexion(ciudad_deportiva, velodromo, 1.26).%cafe
+conexion(velodromo, mixiuhca, 1.06).%cafe
+conexion(mixiuhca, jamaica, 0.95).%cafe
+conexion(jamaica, chabacano, 1.23).%cafe
+conexion(chabacano, lazaro_cardenas, 1.1).%cafe
+conexion(lazaro_cardenas, centro_medico, 1.15).%cafe
+conexion(centro_medico,chilpancingo,1.4).%cafe
+conexion(chilpancingo, patriotismo, 1.1).%cafe
+conexion(patriotismo, tacubaya,1.05).%cafe
+
+%Línea A
+conexion(pantitlan, agricola_oriental,1.58).%morada
+conexion(agricola_oriental, canal_de_san_juan,1.23).%morada
+conexion(canal_de_san_juan,tepalcates,1.6).%morada
+conexion(tepalcates,guelatao,1.3).%morada
+conexion(guelatao, peñon_viejo,2.36).%morada
+conexion(peñon_viejo, acatitla, 1.56).%morada
+conexion(acatitla, santa_marta, 1.24).%morada
+conexion(santa_marta, los_reyes,1.93).%morada
+conexion(los_reyes,la_paz,2.11).%morada
+
+%Línea B
+conexion(buenavista, guerrero, 0.49). %gris-verde
+conexion(guerrero, garibaldi_lagunilla, 0.64). %gris-verde
+conexion(garibaldi_lagunilla, lagunilla, 0.76). %gris-verde
+conexion(lagunilla, tepito, 0.81). %gris-verde
+conexion(tepito, morelos, 0.60). %gris-verde
+conexion(morelos, san_lazaro, 1.33). %gris-verde
+conexion(san_lazaro, flores_magon, 1.17). %gris-verde
+conexion(flores_magon, romero_rubio, 1.06). %gris-verde
+conexion(romero_rubio, oceania, 0.96). %gris-verde
+conexion(oceania, deportivo_oceania, 1.01). %gris-verde
+conexion(deportivo_oceania, bosque_de_aragon, 1.32). %gris-verde
+conexion(bosque_de_aragon, villa_de_aragon, 0.93). %gris-verde
+conexion(villa_de_aragon, nezahualcoyotl, 1.47). %gris-verde
+conexion(nezahualcoyotl, impulsora, 1.55). %gris-verde
+conexion(impulsora, rio_de_los_remedios, 0.60). %gris-verde
+conexion(rio_de_los_remedios, muzquiz, 1.27). %gris-verde
+conexion(muzquiz, ecatepec, 1.69). %gris-verde
+conexion(ecatepec, olimpica, 0.70). %gris-verde
+conexion(olimpica, plaza_aragon, 0.86). %gris-verde
+conexion(plaza_aragon, ciudad_azteca, 0.74). %gris-verde
+
+%Línea 12
+conexion(mixcoac, insurgentes_sur, 1.08). %dorada
+conexion(insurgentes_sur, hospital_20_de_noviembre, 0.77). %dorada
+conexion(hospital_20_de_noviembre, zapata, 0.68). %dorada
+conexion(zapata, parque_de_los_venados, 0.66). %dorada
+conexion(parque_de_los_venados, eje_central, 1.3). %dorada
+conexion(eje_central, ermita, 0.91). %dorada
+conexion(ermita, mexicaltzingo, 2.18). %dorada
+conexion(mexicaltzingo, atlalilco, 1.76). %dorada
+conexion(atlalilco, culhuacan, 1.8). %dorada
+conexion(culhuacan, san_andres_tomatlan, 1.09). %dorada
+conexion(san_andres_tomatlan, lomas_estrella, 1.15). %dorada
+conexion(lomas_estrella, calle_11, 1.09). %dorada
+conexion(calle_11, periferico_oriente, 1.25). %dorada
+conexion(periferico_oriente, tezonco, 1.67). %dorada
+conexion(tezonco, olivos, 0.68). %dorada
+conexion(olivos, nopalera, 1.48). %dorada
+conexion(nopalera, zapotitlan, 1.35). %dorada
+conexion(zapotitlan, tlaltenco, 1.11). %dorada
+conexion(tlaltenco, tlahuac, 1.47). %dorada
